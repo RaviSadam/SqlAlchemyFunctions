54b03d526cadffcf41a2ff07f116282b225c44db
Ravi Sadam <108484600+RaviSadam@users.noreply.github.com>
2023-09-25 22:09:47 +0530

    Update SQLAlchemyFunc.py

--- SQLAlchemyFunc.py
+++ SQLAlchemyFunc.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData,select,func
+from sqlalchemy import create_engine, MetaData, literal,select,func,case,and_
 
 # Create an engine and metadata
 engine = create_engine("mysql+pymysql://root:Ravi123@localhost:3306/study")
@@ -26,11 +26,33 @@ groupConcatQuery = select(
     func.group_concat(studentTable.c.name, ',').label('names')
 ).group_by(studentTable.c.grade)
 
+maxQuery=select(studentTable.c.grade, func.max(studentTable.c.score).label("max score")).group_by(studentTable.c.grade)
+
+minQuery=select(studentTable.c.grade,func.min(studentTable.c.score),func.count()).group_by(studentTable.c.grade)
+
+floorQuery=select(func.floor(3.6))
+
+caseQuery = select(
+    studentTable.c.name,studentTable.c.score,
+    case(
+            (and_(studentTable.c.score>=91,studentTable.c.score<=100),10),
+            (and_(studentTable.c.score>=81, studentTable.c.score<=90) , 9),
+            (and_(studentTable.c.score>=71, studentTable.c.score<=80) , 8),
+            (and_(studentTable.c.score>=61, studentTable.c.score<=70) , 7),
+            (and_(studentTable.c.score>=51, studentTable.c.score<=60) , 6),
+            (and_(studentTable.c.score>=41, studentTable.c.score<=50) , 5),
+            else_=0
+    ).label("Grade Points")
+)
 
 
-#executing the query with Db
+absQuery=select(func.abs(10),literal(10),func.abs(-20),literal(-20),func.abs(30+43-100),literal(30+43-100))
+
+
+
+
 with engine.connect() as connect:
-    #retriving tha data
+    
     avgResult=connect.execute(averageQuery).fetchall()
     print("Average of Student's score is::",avgResult[0][0],"\n")
 
@@ -62,3 +84,31 @@ with engine.connect() as connect:
     print("group concat query result")
     for data in groupConcatResult:
         print(data[0],data[1])
+    
+      
+    result=connect.execute(maxQuery).fetchall()
+    print("grade || max score")
+    #printing the data
+    for data in result:
+      print(data[0],"||",data[1])
+    
+    result=connect.execute(minQuery).fetchall()
+    print("grade | min value | count")
+    #print data
+    for data in result:
+        print(data[0])
+        
+    result=connect.execute(caseQuery).fetchall()
+    for data in result:
+        print(*data)
+    
+
+
+
+
+#executing the query with Db
+with engine.connect() as connect:
+    #retriving tha data
+    result=connect.execute(absQuery).fetchall()
+
+print("Abs values of abs value and actual value::",*result[0])
